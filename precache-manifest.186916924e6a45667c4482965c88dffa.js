1be76f457078d50c0ba657e2cdbf84b0cd99fafa
Andrew Harding <dreweharding@gmail.com>
2018-11-19 18:17:27 -0500

    Updates

---
+++ precache-manifest.186916924e6a45667c4482965c88dffa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cad133fcb65b0a92d568",
+    "url": "/FireflyClicky/static/js/runtime~main.cad133fc.js"
+  },
+  {
+    "revision": "384dbed7a21640685d67",
+    "url": "/FireflyClicky/static/js/main.384dbed7.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/FireflyClicky/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "384dbed7a21640685d67",
+    "url": "/FireflyClicky/static/css/main.3cd9db23.chunk.css"
+  },
+  {
+    "revision": "136af94f94446a52487d5fc4bd8d8432",
+    "url": "/FireflyClicky/index.html"
+  }
+];
